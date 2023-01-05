7dfaf92eb9bc468846754e8d0a7e41c54807366c
Mathys Visticot <mathys.vst@gmail.com>
2023-01-05 15:33:13 +0100

    feat(main): add cookies and other fixes

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-from flask import Flask
-import RPi.GPIO as GPIO
+from flask import Flask, render_template, redirect, url_for, request, make_response
 import time
 
-SERVO_PIN = 37
+app = Flask(__name__)
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(SERVO_PIN, GPIO.OUT)
-p = GPIO.PWM(SERVO_PIN ,50)
+@app.route('/flux')
+def flux():
+    if request.cookies.get('userID'):
+        return render_template('flux.html')
 
-app = Flask(__name__) 
+@app.route('/setcookie', methods=['GET', 'POST'])
+def setCookie():
+    resp = make_response(redirect('/flux'))
+    resp.set_cookie('userID', request.form['username'])
+    return resp
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('flux'))
+    return render_template('login.html', error=error)
+
 @app.route('/')
 def index():
-    p.start(2.5)
-    time.sleep(2)
-    p.ChangeFrequency(10)
-    time.sleep(2)
-    p.ChangeFrequency(2.5)
-    time.sleep(2)
-    p.stop()
-    return "Hello world"
+    return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
